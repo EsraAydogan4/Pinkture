98d5769b5642f8ff6bd8f770941fe1fdb42aaaf5
PNarTN <pinartaylan07@gmail.com@google.com>
2023-05-06 22:39:07 +0300

    060523

--- pinkture/src/App.js
+++ pinkture/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {Add, Chat, Notifications, FavoriteRounded, Person, QuestionMark} from "@mui/icons-material";
+import MenuContainer from './Components/MenuContainer';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menuContainer">
+      <img src="./Pinkture.jpg" width="24" height="24" 
+      alt="Web Sitesi Logosu" />    
+
+        <div className="subMenu">
+          <div>
+            <MenuContainer icon={<Person />}/>
+            <MenuContainer icon={<Notifications />}/>
+            <MenuContainer icon={<Chat />}/>
+          </div>
+          <div>
+            <MenuContainer icon={<FavoriteRounded />}/>
+          </div>
+          <div>
+            <MenuContainer icon={<QuestionMark />}/>
+            <MenuContainer icon={<Add />}/>
+          </div>
+        </div>  
+      </div>
+      <main> </main>
     </div>
   );
 }
+
 
 export default App;

3cf648aacf10e10f2ac83d3a1bad758654e7daf2
Sonsuz Dongu <96468698+Aydgn358@users.noreply.github.com>
2023-07-17 17:10:03 +0300

    .

--- app_api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/loginController');
-const registerController = require('../controllers/registerController');
-
-router.route('/pages/login').post(loginController.girisyap);
-router.route('/pages/register').post(registerController.kayitol);
-
-module.exports=router;
